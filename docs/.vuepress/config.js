1446931e1c320d4fadb8f6266854213b562290b1
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 15:53:51 +0800

    :tada:开始维护

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,4 +4,25 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: 'vue.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
+  base: '/vue-learn/',
+  serviceWorker: false,
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    repo: 'AqingCyan/react.js-learn',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    sidebar: [
+      {
+        title: 'Vue基础',
+        collapsable: false,
+        children: [
+          ['VueBook/', '如何学习？'],
+          'VueBook/StartFromTodoList'
+        ]
+      }
+    ]
+  }
 }
