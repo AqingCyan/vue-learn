27762ce296d3241fbf63a26f2a3b42a9aab0ef89
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 15:31:50 +0800

    :book:学习指南配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'Vue全家桶自学入门指南',
+  description: '一个前端学习者的自我修养',
+  head: [
+    ['link', { rel: 'icon', href: 'vue.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
 }
