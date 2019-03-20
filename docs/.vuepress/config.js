70395e394c0c065257f840da2c11a7e39982861b
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 17:49:46 +0800

    :cookie:配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: 'vue.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/vue-learn/',
+  base: '/vue.js-learn/',
   serviceWorker: false,
   markdown: {
     lineNumbers: true // 代码块显示行号
